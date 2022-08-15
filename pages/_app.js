1578de219f19179958383befa4ca5d70ea62c763
2112miguel <miguel.moreno.angel@gmail.com>
2022-08-15 17:36:05 -0500

    Fix crossOrigin

--- pages/_app.js
+++ pages/_app.js
@@ -14,14 +14,14 @@ function MyApp({ Component, pageProps }) {
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
           rel="stylesheet"
           integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         />
       </Head>
 
       <Script
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-        crossorigin="anonymous"
+        crossOrigin="anonymous"
       />
       <Component {...pageProps} />
     </>
