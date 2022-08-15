8b93276fe16e7cb60dc96908fa24ddcc41b573d1
2112miguel <miguel.moreno.angel@gmail.com>
2022-08-15 17:23:19 -0500

    Fix className

--- pages/Components/Navbar.jsx
+++ pages/Components/Navbar.jsx
@@ -1,12 +1,12 @@
 export const Navbar = () => {
   return (
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container">
-        <a class="navbar-brand" href="#">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand" href="#">
           Portafolio
         </a>
         <button
-          class="navbar-toggler"
+          className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
@@ -14,26 +14,34 @@ export const Navbar = () => {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
         <div
-          class="collapse navbar-collapse justify-content-end"
+          className="collapse navbar-collapse justify-content-end"
           id="navbarNav"
         >
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link text-white" aria-current="page" href="#about">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a
+                className="nav-link text-white"
+                aria-current="page"
+                href="#about"
+              >
                 About
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link text-white" aria-current="page" href="#skills">
+            <li className="nav-item">
+              <a
+                className="nav-link text-white"
+                aria-current="page"
+                href="#skills"
+              >
                 Skills
               </a>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
               <a
-                class="nav-link text-white"
+                className="nav-link text-white"
                 aria-current="page"
                 href="#proyects"
               >
