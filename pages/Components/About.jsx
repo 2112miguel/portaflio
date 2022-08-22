6333285910a27550893a280123763e4691a9ed86
2112miguel <miguel.moreno.angel@gmail.com>
2022-08-22 15:18:42 -0500

    Fix entities

--- pages/Components/About.jsx
+++ pages/Components/About.jsx
@@ -16,7 +16,7 @@ export const About = () => {
           <article className="col-md-5">
             <h1 className="fw-bold">About</h1>
             <p>
-              Hi I'm Miguel, Experienced web developer whith 2 years of
+              Hi I&apos;m Miguel, Experienced web developer whith 2 years of
               experience, strong background as engineer support and network for
               the last 4 years.I studied a bootcamp through 1000+hours, learning
               javaScript, Node.js, React and MongoDB.
