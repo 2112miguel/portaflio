9108745c8e4c6dd3ce0c7ac57bb4ef5ff4da5938
2112miguel <miguel.moreno.angel@gmail.com>
2022-08-22 15:11:12 -0500

    Change class className

--- pages/Components/Skills.jsx
+++ pages/Components/Skills.jsx
@@ -14,22 +14,22 @@ export const Skills = () => {
             <div>
               <h3>Front End</h3>
             </div>
-            <div class="row row-cols-2 ">
-              <div class="col p-4">
-                <i class="bi bi-patch-check-fill mx-3"></i>React
+            <div ClassName="row row-cols-2 ">
+              <div ClassName="col p-4">
+                <i ClassName="bi bi-patch-check-fill mx-3"></i>React
               </div>
-              <div class="col p-4">HTML</div>
-              <div class="col p-4">CSS</div>
-              <div class="col p-4">SASS</div>
+              <div ClassName="col p-4">HTML</div>
+              <div ClassName="col p-4">CSS</div>
+              <div ClassName="col p-4">SASS</div>
             </div>
           </section>
           <section className="col-sm-6 col-9 col-lg-4 p-5 rounded mx-5 text-center">
             <h3>Back End End</h3>
-            <div class="row row-cols-2 ">
-              <div class="col p-4">Node Js</div>
-              <div class="col p-4">C#</div>
-              <div class="col p-4">MongoDb</div>
-              <div class="col p-4">Sql</div>
+            <div ClassName="row row-cols-2 ">
+              <div ClassName="col p-4">Node Js</div>
+              <div ClassName="col p-4">C#</div>
+              <div ClassName="col p-4">MongoDb</div>
+              <div ClassName="col p-4">Sql</div>
             </div>
           </section>
         </article>
