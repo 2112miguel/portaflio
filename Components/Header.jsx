be19848b200df22bf0a0d283923016a32d5e51f0
2112miguel <miguel.moreno.angel@gmail.com>
2022-08-28 16:08:48 -0500

    Fix Responsive

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,17 +1,16 @@
 export default function Header() {
   return (
-    <header className="m-5 p-5">
-      <section className="container">
-        <div className=" ">
-          Name: Miguel Angel
-          <br />
-          Title: Web developer Full Stack
-          <br />
-          Github: 2112miguel
-          <br />
-          Linkedin: miguelanmorenocontreras
-          <br />
-          Email: miguel.moreno.angel@gmail.com
+    <header className="my-5 py-5">
+      <section className="container ">
+        <div className="row ">
+          <ul className="ul-style fs-4">
+            <li>Name: Miguel Angel</li>
+            <li>Title: Web developer Full Stack</li>
+            <li>Github: 2112miguel</li>
+            <li>Linkedin: miguelanmorenocontreras</li>
+            <li>Email: miguel.moreno.angel@gmail.com</li>
+          </ul>
+          <article className="d-flex justify-content-md-center justify-content-sm-start"></article>
         </div>
       </section>
     </header>
