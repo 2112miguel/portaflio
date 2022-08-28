3d04359e700de4217657e8762e3523f2c9ff988b
2112miguel <miguel.moreno.angel@gmail.com>
2022-08-28 16:51:26 -0500

    Change backgroud Color cards

--- Components/Card.jsx
+++ Components/Card.jsx
@@ -22,14 +22,16 @@ export const Card = ({ title, url, urlImg, urlGithub }) => {
           <button
             type="button"
             className="btn btn-outline-primary"
-            onClick={() => (location.href = url)}
+            onClick={() => window.open(url, "_blank", "noopener,noreferrer")}
           >
             Online
           </button>
           <button
             type="button"
             className="btn btn-outline-secondary ms-3"
-            onClick={() => (location.href = urlGithub)}
+            onClick={() =>
+              window.open(urlGithub, "_blank", "noopener,noreferrer")
+            }
           >
             Github
           </button>
