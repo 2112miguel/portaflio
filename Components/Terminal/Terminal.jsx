09fa9b3b601630dd1a0cc49e2f483e37ac9d104f
2112miguel <miguel.moreno.angel@gmail.com>
2022-10-16 12:06:44 -0500

    Add Terminal component

---
+++ Components/Terminal/Terminal.jsx
@@ -0,0 +1,21 @@
+//import "./Terminal.css";
+
+export const Terminal = () => {
+  return (
+    <section>
+      <div className="console">
+        <header>
+          <p>miguel@portafolio</p>
+        </header>
+        <article className="consolebody">
+          <p>{">"} portafolio@name:~$ Miguel Angel</p>
+          <p>{">"} portafolio@title:~$ Full Stack Developer</p>
+          <p>{">"} portafolio@github:~$ 2112miguel</p>
+          <p>{">"} portafolio@linkedin:~$ miguelanmorenocontreras</p>
+          <p>{">"} portafolio@email:~$ miguel.moreno.angel@gmail.com</p>
+          <p>{">"} </p>
+        </article>
+      </div>
+    </section>
+  );
+};
