d810931408a4a09e86a1e6461248be45c5b09515
2112miguel <miguel.moreno.angel@gmail.com>
2022-10-16 12:07:55 -0500

    Modify and add component Terminal

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,18 +1,13 @@
+import { Terminal } from "./Terminal/Terminal";
+
 export default function Header() {
   return (
-    <header className="my-5 py-5" id="home">
-      <section className="container ">
-        <div className="row ">
-          <ul className="ul-style fs-4">
-            <li>Name: Miguel Angel</li>
-            <li>Title: Web developer Full Stack</li>
-            <li>Github: 2112miguel</li>
-            <li>Linkedin: miguelanmorenocontreras</li>
-            <li>Email: miguel.moreno.angel@gmail.com</li>
-          </ul>
-          <article className="d-flex justify-content-md-center justify-content-sm-start"></article>
-        </div>
-      </section>
-    </header>
+    <div>
+      <header className="my-5 py-5" id="home">
+        <section className="container ">
+          <Terminal />
+        </section>
+      </header>
+    </div>
   );
 }
