417aeee90aa41873a7ae09107bd07dae08e40753
2112miguel <miguel.moreno.angel@gmail.com>
2022-10-24 11:54:09 -0500

    Add to Header welcome

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,13 +1,39 @@
 import { Terminal } from "./Terminal/Terminal";
+import Typewriter from "typewriter-effect";
 
 export default function Header() {
   return (
     <div>
-      <header className="my-5 py-5" id="home">
+      <section className="my-5 py-5" id="home">
         <section className="container ">
+          <div className="m-5 p-5">
+            <h1>
+              <Typewriter
+                onInit={(typewriter) => {
+                  typewriter
+                    .pauseFor(500)
+                    .typeString("Welcome to my ")
+                    .pauseFor(300)
+
+                    .typeString(
+                      "<strong>Portafolio</strong> where I share my ideas"
+                    )
+                    .deleteChars(33)
+                    .typeString(
+                      "<strong>space</strong> where I share my projects"
+                    )
+                    .pauseFor(1000)
+                    .start();
+                }}
+                options={{
+                  delay: 75,
+                }}
+              />
+            </h1>
+          </div>
           <Terminal />
         </section>
-      </header>
+      </section>
     </div>
   );
 }
